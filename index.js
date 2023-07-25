e8dbb00e2310510206f672bc7b33aef90e063fa7
Ujjwal Verma <ujjawalverma77@gmail.com>
2023-07-26 02:21:18 +0530

    inital commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Start your es6 scripts here
+import express from "express";
+import resolvers from "./resolvers";
+import schema from "./schema";
+import { graphqlHTTP } from "express-graphql";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Up and running with graphql");
+});
+
+const root = resolvers;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+    rootValue: root,
+  })
+);
+
+app.listen(3000, () => {
+  console.log("Running at 3000");
+});
